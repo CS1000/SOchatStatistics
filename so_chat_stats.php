1b96182d7309f15f3abb8fefd9c5e96b0225beef
Vaibhav Mehta <firekillz@gmail.com>
2014-09-12 19:43:24 +0530

    Used shorthand
    shorthand php syntax to echo

--- so_chat_stats.php
+++ so_chat_stats.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>Room <?php echo $room; ?> Statistics</title>
+    <title>Room <?=$room?> Statistics</title>
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript">
       google.load("visualization", "1", {packages:["corechart"]});
@@ -13,7 +13,7 @@
         ]);
   
         var options = {
-          title: 'Ocurence of word "<?=$word?>" in room <?php echo $room; ?>'
+          title: 'Ocurence of word "<?=$word?>" in room <?=$room?>'
         };
   
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
