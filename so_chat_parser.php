7763c8dbc5fffec8dfd45410a653fc1a5a7bad23
godka <thegodka@yahoo.com>
2014-09-19 21:55:22 +0300

    range improvements

--- so_chat_parser.php
+++ so_chat_parser.php
@@ -10,12 +10,23 @@
   $data=file('http://chat.stackoverflow.com/search?q='.urlencode($word).'&Room='.$room.'&page=1&pagesize=100&sort=newest');
   
   $start=preg_grep('~<div class="timestamp">~i', $data);
-  $end=array_pop($start);
-  $start=array_shift($start);
+  $end=trim(strip_tags(array_shift($start)));
+  if (count($start)<1) $start=$end;
+  else $start=trim(strip_tags(array_pop($start)));
+  $start=str_replace("'", '20', $start);
+  $end=str_replace("'", '20', $end);
+  $start=new DateTime($start);
+  $end=new DateTime($end);
+  $interval=$start->diff($end);
+  $interval=$interval->days;
+  $range=$interval.'days, ';
+  if ($interval>0) $range.='from '.$start->format("d M Y").' to '.$end->format("d M Y");
+  else $range.=$start->format("d M Y");
 
-  $messageNumber=preg_grep('~<p>([0-9]+) messages found</p>~i', $data);
+
+  $messageNumber=preg_grep('~<p>([0-9]+) messages? found</p>~i', $data);
   preg_match('~>([0-9]+)~', array_pop($messageNumber), $allTimeMsg);
-  $messageNumber=$allTimeMsg[1];
+  $messageNumber=(int)$allTimeMsg[1];
 
   $roomname=preg_grep('~class="searchroom~i', $data);
   $roomname=each($roomname);
@@ -23,13 +34,15 @@
   $roomname=$matches[1];
 
   $users=preg_grep('~<div class="username"><a href="/users/~', $data);
-  
+
   $count=array();
   $names=array();
+  $shownMessages=0;
   foreach ($users as $user) { 
       preg_match('~/users/(\d+)/[^>]+>([^<]+)~', $user, $m);
       $names[$m[1]]=$m[2];
       @$count[$m[1]]++;
+      $shownMessages++;
   }
   arsort($count);
   $list='';
--- so_chat_stats.php
+++ so_chat_stats.php
@@ -28,7 +28,7 @@
         color: #777777;
         font-size: x-small;
       }
-      .timestamp {
+      .highlight {
         display: inline;
         color: #009933;
       }
@@ -38,22 +38,17 @@
     </style>
   </head>
   <body>
-    <h1>Ocurence of word "<span class="timestamp"><?=$word?></span>" in 
-      "<span class="timestamp"><?=$roomname?></span>" room</h1>
+    <h1>Ocurence of "<span class="highlight"><?=$word?></span>" in 
+      "<span class="highlight"><?=$roomname?></span>" room</h1>
     <div id="piechart" style="width: 900px; height: 500px;"></div><br>
     <div id="notices">
       <?php 
-        $shownMessages=count($users);
-        echo 'Period range: ';
-        if ($start!='') echo $start,' to ';
-        else echo '(one day) ';
-        echo $end,'<br>'; 
-
+        echo 'Period range: ',$range,'<br>'; 
         echo '<p>Showing statistics with ';
         $prc=round($shownMessages*100/$messageNumber, 1);
-        if ((int)$prc==100 or $start=='') echo '<b>all time data</b> ';
+        if ((int)$prc==100) echo '<b>all time data</b> ';
         else echo '<b>recent data</b> (',$prc,'% of all time) ';
-        echo 'where word "',$word,'" occured.</p>';
+        echo 'where searchphrase "',$word,'" occured.</p>';
       ?>
       <p class="notice">* users with word count less than 2 are not shown in the list, 
         and counted totards "Others" group.</p>
