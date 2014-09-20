c537d7b04ef798da293fb0d0de62b54b0b50d011
Vaibhav Mehta <firekillz@gmail.com>
2014-09-20 12:51:51 +0530

    Better design
    Also removed few bits here and there...

--- so_chat_stats.php
+++ so_chat_stats.php
@@ -1,8 +1,30 @@
 <?php require('so_chat_parser.php');?>
 <html>
   <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" />
     <title><?=$roomname?> - Room Statistics</title>
+    <link rel="stylesheet" href="/default.css" />
+  </head>
+  <body>
+    <div id="wrapper" class="clear">
+      <section id="piechart"></section>
+      <section>
+         <h1>Occurrence of "<span class="highlight"><?=$word?></span>" in "<span class="highlight"><?=$roomname?></span>" room</h1>
+         <div id="notices">
+          <table>
+            <tr>
+              <td>Period range:</td>
+              <td><?=$range?></td>
+            </tr>
+            <tr>
+              <td>Statistics</td>
+              <td><?=($allTimePercent==100) ? '<b>All time data</b> where "' : '<b>recent data</b> ('.$prc.'% of all time) where search phrase "',$word,'" occurred.';?></td>
+            </tr>
+          </table>
+          <p class="notice">* Users with word count less than 2 are not shown in the list, and counted totards "Others" group.</p>
+        </div>
+      </section>
+    </div>
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript">
       google.load("visualization", "1", {packages:["corechart"]});
@@ -20,38 +42,5 @@
         chart.draw(data, options);
       }
     </script>
-    <style type="text/css">
-      body {
-        font-family: Verdana,Arial,sans-serif;
-      }
-      .notice {
-        color: #777777;
-        font-size: x-small;
-      }
-      .highlight {
-        display: inline;
-        color: #009933;
-      }
-      h1 {
-        font-size: x-large;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Ocurence of "<span class="highlight"><?=$word?></span>" in 
-      "<span class="highlight"><?=$roomname?></span>" room</h1>
-    <div id="piechart" style="width: 900px; height: 500px;"></div><br>
-    <div id="notices">
-      <?php 
-        echo 'Period range: ',$range,'<br>'; 
-        echo '<p>Showing statistics with ';
-        
-        if ($allTimePercent==100) echo '<b>all time data</b> ';
-        else echo '<b>recent data</b> (',$prc,'% of all time) ';
-        echo 'where searchphrase "',$word,'" occured.</p>';
-      ?>
-      <p class="notice">* users with word count less than 2 are not shown in the list, 
-        and counted totards "Others" group.</p>
-    </div>
   </body>
 </html>
